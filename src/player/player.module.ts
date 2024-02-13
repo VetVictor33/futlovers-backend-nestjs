a2d22bbbaaa9311028513277c497e9cd85577c2d
Victor Feliciano <victorjfeliciano@gmail.com>
2024-02-13 11:17:12 -0300

    fix: remove unnecessary provide of global module

--- src/player/player.module.ts
+++ src/player/player.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'prisma/prisma.service';
 import { PlayerController } from './player.controller';
 import { PlayerRepository } from './player.repository';
 import { PlayerService } from './player.service';
 
 @Module({
-  providers: [PlayerService, PrismaService, PlayerRepository],
+  providers: [PlayerService, PlayerRepository],
   controllers: [PlayerController]
 })
 export class PlayerModule {}
--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from "prisma/prisma.service";
 import { TeamController } from './team.controller';
 import { TeamRepository } from "./team.repository";
 import { TeamService } from './team.service';
 
 @Module({
-  providers: [TeamService, PrismaService, TeamRepository],
+  providers: [TeamService, TeamRepository],
   controllers: [TeamController]
 })
 export class TeamModule {}
