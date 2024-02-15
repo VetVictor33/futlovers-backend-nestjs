71855186a1d3c5ffdf8aa6a9ad7f2f48e6b786c5
Victor Feliciano <victorjfeliciano@gmail.com>
2024-02-15 11:33:21 -0300

    feat:  using mocked data from mocks directory

--- src/player/player.service.spec.ts
+++ src/player/player.service.spec.ts
@@ -3,38 +3,20 @@ import { PlayerService } from './player.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { PlayerRepository } from '../repositories/player.repository';
 import { Player } from '@prisma/client';
-
-interface PrismaServiceMock {
-  player: {
-    create: jest.MockedFn<any>;
-    findMany: jest.MockedFn<any>;
-    findUnique: jest.MockedFn<any>;
-    update: jest.MockedFn<any>;
-    delete: jest.MockedFn<any>;
-  };
-}
-
-const prismaMock = {
-    player: {
-        create: jest.fn(),
-        findMany: jest.fn(),
-        findUnique: jest.fn(),
-        update: jest.fn(),
-        delete: jest.fn(),
-    }
-};
+import { IPrismaServiceMock, PrismaServiceMock } from '../../test/mocks/prisma-mock';
+import { playersList, singlePlayer } from '../../test/mocks/players-mocks';
 
 describe('PlayerService', () => {
   let service: PlayerService;
-  let prismaService: PrismaServiceMock
+  let prismaService: IPrismaServiceMock
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlayerService, PlayerRepository, {provide: PrismaService, useValue: prismaMock}],
+      providers: [PlayerService, PlayerRepository, {provide: PrismaService, useValue: PrismaServiceMock}],
     }).compile();
 
     service = module.get<PlayerService>(PlayerService);
-    prismaService = module.get<PrismaService>(PrismaService) as unknown as PrismaServiceMock;
+    prismaService = module.get<PrismaService>(PrismaService) as unknown as IPrismaServiceMock;
   });
 
   it('should be defined', () => {
@@ -42,49 +24,20 @@ describe('PlayerService', () => {
   });
 
   it('should create a player', async () => {
-    const team_id = 'team_id_123'
-    const playerData: Partial<Player> = {
-        name: 'Fulano',
-        age: 25,
-    };
+    const result = await service.create(singlePlayer.team_id, {name: singlePlayer.name, age: singlePlayer.age});
 
-    const createdPlayer: Partial<Player> = {
-        id: 'player_id_123',
-        name: 'Fulano',
-        age: 25,
-        team_id
-    };
-
-    prismaService.player.create.mockResolvedValue(createdPlayer)
-
-    const result = await service.create(team_id, playerData);
-
-    expect(result).toEqual(createdPlayer);
-    expect(prismaService.player.create).toHaveBeenCalledWith({ data: {...playerData, team_id} });
+    expect(result).toEqual(singlePlayer);
+    expect(prismaService.player.create).toHaveBeenCalledWith({ data: {name: singlePlayer.name, age: singlePlayer.age, team_id: singlePlayer.team_id} });
   });
 
   it('should list a player by id', async () => {
-    const playersList: Player[] = [
-      { id: '1', name: 'Player 1', age: 20, team_id: 'team_1_id', created_at: new Date(),  updated_at: new Date()},
-      { id: '2', name: 'Player 2', age: 22, team_id: 'team_2_id', created_at: new Date(),  updated_at: new Date() },
-    ];
+    const result = await service.findById(singlePlayer.id)
   
-    prismaService.player.findUnique.mockResolvedValue(playersList[0])
-  
-    const result = await service.findById('1')
-  
-    expect(result).toBe(playersList[0])
-    expect(prismaService.player.findUnique).toHaveBeenCalledWith({where: {id: '1'}})
+    expect(result).toBe(singlePlayer)
+    expect(prismaService.player.findUnique).toHaveBeenCalledWith({where: {id: singlePlayer.id}})
   })
   
   it('should list all players', async () => {
-    const playersList: Player[] = [
-      { id: '1', name: 'Player 1', age: 20, team_id: 'team_1_id', created_at: new Date(),  updated_at: new Date()},
-      { id: '2', name: 'Player 2', age: 22, team_id: 'team_2_id', created_at: new Date(),  updated_at: new Date() },
-    ];
-  
-    prismaService.player.findMany.mockResolvedValue(playersList)
-  
     const result = await service.findAll()
   
     expect(result).toBeInstanceOf(Array<Player>)
@@ -93,16 +46,12 @@ describe('PlayerService', () => {
   })
 
   it('should update a player', async () => {
-    const updatedPlayer: Partial<Player> = {
-      name: 'New Name', age: 20, team_id: 'team_1_id'
-    }
+    const result = await service.update(singlePlayer.id, {name: singlePlayer.name, age: singlePlayer.age, team_id: singlePlayer.team_id})
 
-    prismaService.player.update.mockResolvedValue(updatedPlayer)
-
-    const result = await service.update('1', updatedPlayer)
-
-    expect(result).toBe(updatedPlayer)
-    expect(prismaService.player.update).toHaveBeenCalledWith({where: {id: '1'}, data: {...updatedPlayer}})
+    expect(result).toStrictEqual(singlePlayer)
+    expect(prismaService.player.update).toHaveBeenCalledWith(
+      {where: {id: singlePlayer.id}, data: {name: singlePlayer.name, age: singlePlayer.age, team_id: singlePlayer.team_id}
+    })
     
   })
 
